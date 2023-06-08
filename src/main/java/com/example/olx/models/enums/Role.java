cfbed28b129a8cbdedd3174249ab21d9a19b10a6
Abdulaziz Baisarov <baisarov.abdulaziz@gmail.com>
2023-06-08 16:48:53 +0600

    fixed name of role moderator

--- src/main/java/com/example/olx/models/enums/Role.java
+++ src/main/java/com/example/olx/models/enums/Role.java
@@ -3,7 +3,7 @@ package com.example.olx.models.enums;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    ROLE_USER, ROLE_ADMIN, ROLE_MODER;
+    ROLE_USER, ROLE_ADMIN, ROLE_MODERATOR;
 
     @Override
     public String getAuthority() {
