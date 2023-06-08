3a7284d91fcd44f7821ca81e26989cebaf23b41b
Abdulaziz Baisarov <baisarov.abdulaziz@gmail.com>
2023-06-08 18:45:17 +0600

    chagned pom and maven

--- src/main/java/com/example/olx/models/enums/Role.java
+++ src/main/java/com/example/olx/models/enums/Role.java
@@ -3,7 +3,7 @@ package com.example.olx.models.enums;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    ROLE_USER, ROLE_ADMIN, ROLE_MODERATOR;
+    ROLE_USER, ROLE_ADMIN, ROLE_MODERATOR, ROLE_ASF;
 
     @Override
     public String getAuthority() {
